40e28518620ffbbb4665cee2cd50f9bcbd4a469b
Ngoc <thnhngc17@gmail.com>
2023-07-28 11:12:13 +0700

    Initial Commit

---
+++ Aspire Vogue/Aspire_VogueApp.swift
@@ -0,0 +1,17 @@
+//
+//  Aspire_VogueApp.swift
+//  Aspire Vogue
+//
+//  Created by Duong Vu Thanh Ngoc on 28/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct Aspire_VogueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Aspire VogueUITests/Aspire_VogueUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Aspire_VogueUITestsLaunchTests.swift
+//  Aspire VogueUITests
+//
+//  Created by Duong Vu Thanh Ngoc on 28/07/2023.
+//
+
+import XCTest
+
+final class Aspire_VogueUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
